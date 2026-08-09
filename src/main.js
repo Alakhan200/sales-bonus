f0c8d07a92b4394a1614af7e55fde93c5e5f5e46
Alakhan <topvizet@gmail.com>
2026-08-09 13:04:09 +0300

    почти закончил 1 этап проектирования

--- src/main.js
+++ src/main.js
@@ -5,6 +5,14 @@
  * @returns {number}
  */
 function calculateSimpleRevenue(purchase, _product) {
+    const { discount, sale_price, quantity } = purchase;
+
+
+    const discountDecimal = discount / 100;
+    const fullPrice = sale_price * quantity;
+    const revenue = fullPrice * (1 - discountDecimal);
+    
+    return revenue;
    // @TODO: Расчет выручки от операции
 }
 
@@ -14,10 +22,47 @@ function calculateSimpleRevenue(purchase, _product) {
  * @param total общее число продавцов
  * @param seller карточка продавца
  * @returns {number}
+ * 
  */
-function calculateBonusByProfit(index, total, seller) {
-    // @TODO: Расчет бонуса от позиции в рейтинге
+
+function calculateBonuses(sellers, bonusStrategy) {
+    const sorted = [...sellers].sort((a, b) => b.profit - a.profit);
+
+    const total = sorted.length;
+
+    return sorted.map((seller, index) => {
+    const bonusPercent = bonusStrategy(index, total, seller);
+
+    const bonusAmount = seller.profit * (bonusPercent / 100);
+
+    return {
+    ...seller,
+    bonusPercent,
+    bonusAmount,
+    };
+});
 }
+
+function calculateBonusByProfit(index, total, seller) {
+const { profit } = seller;
+
+// 1 место (индекс 0) → 15%
+if (index === 0) {
+    return 15;
+}
+
+// 2 и 3 место (индексы 1 и 2) → 10%
+if (index === 1 || index === 2) {
+    return 10;
+}
+
+// Последнее место (индекс total - 1) → 0%
+if (index === total - 1) {
+    return 0;
+}
+
+// Все остальные → 5%
+return 5;}
 
 /**
  * Функция для анализа данных продаж
@@ -26,8 +71,8 @@ function calculateBonusByProfit(index, total, seller) {
  * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
  */
 function analyzeSalesData(data, options) {
-    // @TODO: Проверка входных данных
-
+    
+    const { calculateRevenue, calculateBonus } = options;
     // @TODO: Проверка наличия опций
 
     // @TODO: Подготовка промежуточных данных для сбора статистики
